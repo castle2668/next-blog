95d3bad31732dbc2fbad1d3abb4b2eed02c94229
Sean Huang <huang.sean@icp-si.com>
2025-01-16 15:33:52 +0800

    Added Next.js + Shadcn UI and ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,32 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import prettier from 'eslint-plugin-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const config = [
+  ...compat.extends('next/core-web-vitals', 'prettier'),
+  {
+    plugins: {
+      prettier,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+]
 
-export default eslintConfig;
+export default config
