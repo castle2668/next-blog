43547a9ff2c1ab7f80dd115f167f0da6b7bc553a
Sean Huang <castle2668@gmail.com>
2025-02-04 12:23:12 +0800

    Update icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,17 +26,17 @@ export const metadata: Metadata = {
     description:
       '歡迎來到我的部落格，這裡有我分享的技術筆記、生活點滴和旅遊見聞。',
     siteName: '大貓的第一個家',
-    // images: [
-    //   {
-    //     url: 'https://seanhuang.dev/og.png',
-    //   },
-    // ],
+    images: [
+      {
+        url: 'https://seanhuang.dev/logo.jpeg',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     site: '@castle2668',
     creator: '@castle2668',
-    // images: 'https://seanhuang.dev/og.png',
+    images: 'https://seanhuang.dev/logo.jpeg',
   },
 }
 
